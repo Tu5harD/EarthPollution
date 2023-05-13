f1e99a609587d8136defce5e493d2ead28c30017
Tushar Dukane <tushardukane@Tushars-MacBook-Air.local>
2023-05-13 15:07:08 +0530

    Updates

---
+++ precache-manifest.be5abcd3ac3cd68901cde0c07c8b0906.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baee88f32e58633a3d45",
+    "url": "/EarthPollution/static/css/main.6ff3ca85.chunk.css"
+  },
+  {
+    "revision": "baee88f32e58633a3d45",
+    "url": "/EarthPollution/static/js/main.baee88f3.chunk.js"
+  },
+  {
+    "revision": "2a755652835a5d893746",
+    "url": "/EarthPollution/static/js/1.2a755652.chunk.js"
+  },
+  {
+    "revision": "757623e7b54e2e590bcd",
+    "url": "/EarthPollution/static/js/runtime~main.757623e7.js"
+  },
+  {
+    "revision": "58ea6dc1d41edc9d93ed5f302bfe5da4",
+    "url": "/EarthPollution/index.html"
+  }
+];
